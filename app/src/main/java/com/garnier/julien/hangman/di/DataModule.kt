8aa7bde3af6a899a81fead454045201cb146a493
JulienG <legroscondu53@gmail.com>
2024-02-08 22:34:16 +0100

    Create data module for database / dao injection

---
+++ app/src/main/java/com/garnier/julien/hangman/di/DataModule.kt
@@ -0,0 +1,24 @@
+package com.garnier.julien.hangman.di
+
+import android.app.Application
+import androidx.room.Room
+import com.garnier.julien.hangman.database.HangManDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataModule {
+
+    @Provides
+    @Singleton
+    fun provideHangManDatabase(application: Application): HangManDatabase {
+        return Room
+            .databaseBuilder(application, HangManDatabase::class.java, "hangman_database.db")
+            .build()
+    }
+
+}
