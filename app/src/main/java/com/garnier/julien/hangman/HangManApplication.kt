dbbe2757c2a1dfd1fbc0b33b16f034c92a607765
JulienG <legroscondu53@gmail.com>
2024-02-08 22:29:20 +0100

    Create Application class

---
+++ app/src/main/java/com/garnier/julien/hangman/HangManApplication.kt
@@ -0,0 +1,7 @@
+package com.garnier.julien.hangman
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp(Application::class)
+class HangManApplication: Application()
