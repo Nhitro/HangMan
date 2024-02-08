c841bf16a79f2aff53339a16199534863451090d
JulienG <legroscondu53@gmail.com>
2024-02-08 23:54:39 +0100

    Bind letters state to list

--- app/src/main/java/com/garnier/julien/hangman/ui/MainActivity.kt
+++ app/src/main/java/com/garnier/julien/hangman/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.garnier.julien.hangman.R
 import com.garnier.julien.hangman.databinding.ActivityMainBinding
+import com.garnier.julien.hangman.ui.letter.LetterAdapter
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,9 +18,16 @@ class MainActivity : AppCompatActivity() {
                 .inflate(layoutInflater)
                 .also { setContentView(it.root) }
 
+        val adapter = LetterAdapter()
+
+        // Init view
+        binding.lettersToGuessRecyclerView.adapter = adapter
+
         mainViewModel
             .getCurrentGameStatusLiveData()
             .observe(this) {
+                // Update view according VM state
+                adapter.submitList(it.lettersAlreadyGuessed)
                 binding.victoryNumber.text =
                     resources.getString(R.string.number_of_victories, it.numberOfVictories)
                 binding.gamesNumber.text =
