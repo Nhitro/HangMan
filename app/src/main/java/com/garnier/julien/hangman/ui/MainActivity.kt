ee2663f3434b604502e9b45402ee74008cd971bc
JulienG <legroscondu53@gmail.com>
2024-02-08 23:40:31 +0100

    Bind view model state to UI

--- app/src/main/java/com/garnier/julien/hangman/ui/MainActivity.kt
+++ app/src/main/java/com/garnier/julien/hangman/ui/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.garnier.julien.hangman.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.AndroidViewModel
+import androidx.appcompat.app.AppCompatActivity
 import com.garnier.julien.hangman.R
+import com.garnier.julien.hangman.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val viewModel: AndroidViewModel by viewModels()
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding =
+            ActivityMainBinding
+                .inflate(layoutInflater)
+                .also { setContentView(it.root) }
+
+        mainViewModel
+            .getCurrentGameStatusLiveData()
+            .observe(this) {
+                binding.victoryNumber.text =
+                    resources.getString(R.string.number_of_victories, it.numberOfVictories)
+                binding.gamesNumber.text =
+                    resources.getString(R.string.number_of_games, it.numberOfGames)
+                binding.gameDescription.text =
+                    if (it.isGameOver) getString(R.string.game_is_over)
+                    else resources.getString(R.string.game_state_description, it.numberOfTriesLeft)
+            }
     }
 }
